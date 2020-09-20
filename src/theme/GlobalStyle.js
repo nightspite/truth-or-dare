121744acfa478bdaab042391b6a62dfb8c0bdd0c
nightspite <we4rpl2@gmail.com>
2020-09-20 23:24:07 +0200

    🧰 prepared for app refactoring

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,24 @@
+import { createGlobalStyle, css } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`${css`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    padding: 0;
+    font-size: 1.6rem;
+    font-family: "Montserrat", sans-serif;
+    margin: 0;
+  }
+`}`;
+
+export default GlobalStyle;
